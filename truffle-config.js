ea8b8ec3eb4afc1fcfc14153c8d8e6f3f72c2234
sollcyrpto <74764407+sollcyrpto@users.noreply.github.com>
2021-03-11 00:53:59 +0200

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,13 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
-  networks: {
-    development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard BSC port (default: none)
-      network_id: "*",       // Any network (default: none)
-    },
-    testnet: {
+    networks: {
+        development: {
+            host: '127.0.0.1',
+            port: 8545,
+            network_id: '*',
+        },
+      
+      testnet: {
       provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
       network_id: 97,
       confirmations: 10,
@@ -23,16 +21,22 @@ module.exports = {
       timeoutBlocks: 200,
       skipDryRun: true
     },
-  },
+    },
 
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-  },
+    mocha: {
+        reporter: 'eth-gas-reporter',
+    },
 
-  // Configure your compilers
-  compilers: {
     solc: {
-    }
-  }
-}
+        optimizer: {
+            enabled: true,
+            runs: 200,
+        },
+    },
+
+    compilers: {
+        solc: {
+            version: '0.5.2',
+        },
+    },
+};
